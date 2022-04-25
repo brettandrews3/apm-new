8da3e42743603e79a7c1975baf971f952528095c
Brett A <62891439+brettandrews3@users.noreply.github.com>
2022-04-25 09:57:46 -0500

    PS Angular 5.5 - Binding with Interpolation
    Added pageTitle to exported class ProductListComponent in product-list,.component.ts. I interpolated this in line 3 on the HTML file of the same name.

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -4,6 +4,10 @@ import { Component } from "@angular/core";
   selector: 'pm-products',
   templateUrl: './product-list.component.html'
 })
-export class ProductListComponent {
 
+// PS Angular 5.5 - Binding w/ Interpolation
+// pageTitle is bound to the HTML of same name, replacing
+// 'Product List' at line 3 w/ the value below.
+export class ProductListComponent {
+  pageTitle: string = 'Product List';
 }
