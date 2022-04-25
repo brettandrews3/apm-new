df8ceae8f8051fe6581b19702d463c451ad10b6a
Brett A <62891439+brettandrews3@users.noreply.github.com>
2022-04-25 10:22:30 -0500

    PS Angular 5.6 - Adding Logic with Directives
    Create product[] with type of 'any' and pass the products in. *ngIf, Angular's built-in structural directive, allows the products[] to be displayed if there's products in it (i.e., length == true). This directive is on line 18 in the HTML file.

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -8,6 +8,65 @@ import { Component } from "@angular/core";
 // PS Angular 5.5 - Binding w/ Interpolation
 // pageTitle is bound to the HTML of same name, replacing
 // 'Product List' at line 3 w/ the value below.
+
+// PS Angular 5.6 - Adding Logic w/ Directives
+// Create product[] with type of 'any' and pass the
+// products in. *ngIf, Angular's built-in structural
+// directive, allows the products[] to be displayed
+// if there's products in it (i.e., length == true).
+// This directive is on line 18 in the HTML file.
 export class ProductListComponent {
   pageTitle: string = 'Product List';
+  products: any[] = [
+      {
+        "productId": 1,
+        "productName": "Leaf Rake",
+        "productCode": "GDN-0011",
+        "releaseDate": "March 19, 2021",
+        "description": "Leaf rake with 48-inch wooden handle.",
+        "price": 19.95,
+        "starRating": 3.2,
+        "imageUrl": "assets/images/leaf_rake.png"
+      },
+      {
+        "productId": 2,
+        "productName": "Garden Cart",
+        "productCode": "GDN-0023",
+        "releaseDate": "March 18, 2021",
+        "description": "15 gallon capacity rolling garden cart",
+        "price": 32.99,
+        "starRating": 4.2,
+        "imageUrl": "assets/images/garden_cart.png"
+      },
+      {
+        "productId": 5,
+        "productName": "Hammer",
+        "productCode": "TBX-0048",
+        "releaseDate": "May 21, 2021",
+        "description": "Curved claw steel hammer",
+        "price": 8.9,
+        "starRating": 4.8,
+        "imageUrl": "assets/images/hammer.png"
+      },
+      {
+        "productId": 8,
+        "productName": "Saw",
+        "productCode": "TBX-0022",
+        "releaseDate": "May 15, 2021",
+        "description": "15-inch steel blade hand saw",
+        "price": 11.55,
+        "starRating": 3.7,
+        "imageUrl": "assets/images/saw.png"
+      },
+      {
+        "productId": 10,
+        "productName": "Video Game Controller",
+        "productCode": "GMG-0042",
+        "releaseDate": "October 15, 2020",
+        "description": "Standard two-button video game controller",
+        "price": 35.95,
+        "starRating": 4.6,
+        "imageUrl": "assets/images/xbox-controller.png"
+      }
+  ];
 }
