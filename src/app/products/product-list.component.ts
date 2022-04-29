b783fec8e8c70d71be51864aaba8c165a5efb3c8
Brett A <62891439+brettandrews3@users.noreply.github.com>
2022-04-29 16:34:01 -0500

    PS Angular 8.4 - Passing Data to Component w/ @Input

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -8,7 +8,8 @@ import { IProduct } from "./product";
 })
 
 // Angular 8.2 - Building a Nested Component
-// Angular 8.3  Using a Nested Component
+// Angular 8.3 - Using a Nested Component
+// Angular 8.4 - Passing Data to a Nested Component w/ @Input
 
 export class ProductListComponent implements OnInit {
   pageTitle: string = 'Product List';
