7099b869411c4d7d83944b3ee9cbf7e65fb22096
Brett A <62891439+brettandrews3@users.noreply.github.com>
2022-05-26 15:01:33 -0500

    PS Angular 13.10 - Shared Module
    We created the shared.module.ts file and imported the product module from the last commit by using the Angular CLI like so:
    
    ng g m shared/shared --flat -m products/product.module
    
    The shared module is a clearinghouse for many of the application modules we may want to use in the program.

--- src/app/products/product.module.ts
+++ src/app/products/product.module.ts
@@ -1,23 +1,18 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { ProductListComponent } from './product-list.component';
 import { ProductDetailComponent } from './product-detail.component';
-import { StarComponent } from '../shared/star.component';
 import { ConvertToSpacesPipe } from '../shared/convert-to-spaces.pipe';
-import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { ProductDetailGuard } from './product-detail.guard';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [
     ProductListComponent,
     ProductDetailComponent,
     ConvertToSpacesPipe,
-    StarComponent
   ],
   imports: [
-    CommonModule,
-    FormsModule,
     RouterModule.forChild([
       { path: 'products', component: ProductListComponent },
       // PS Angular 12.6: this path hooks up guard to the route set in product-detail.guard.ts:
@@ -26,7 +21,8 @@ import { ProductDetailGuard } from './product-detail.guard';
         canActivate: [ProductDetailGuard],
         component: ProductDetailComponent
       },
-    ])
+    ]),
+    SharedModule
   ]
 })
 export class ProductModule { }
